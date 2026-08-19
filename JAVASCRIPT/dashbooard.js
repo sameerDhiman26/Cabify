dc217a6c10fc1d8e39c2ef1ffa2cabf36ce5fd56
dishantdubey0017-coder <dishantdubey0017@gmail.com>
2026-08-19 14:33:12 +0530

    dishant

---
+++ JAVASCRIPT/dashbooard.js
@@ -0,0 +1,24 @@
+let homePage = document.querySelector(".homepage");
+let findRidePage = document.querySelector(".findRide");
+let sidebar = document.querySelector(".sidebar");
+
+
+sidebar.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains("home-btn")){
+        findRidePage.style.display = "none";
+        homePage.style.display = "block";
+        document.querySelectorAll(".menu-item").forEach((menu)=>{
+            menu.classList.remove("active")
+        })
+         e.target.classList.add("active")
+    }
+    if(e.target.classList.contains("find-btn")){
+        findRidePage.style.display = "block";
+        homePage.style.display = "none";
+        document.querySelectorAll(".menu-item").forEach((menu)=>{
+            menu.classList.remove("active")
+        })
+        e.target.classList.add("active")
+    }
+})
