7aac2e0cdd21d4d436f6e6f67ae8a83c007ba4ea
dishantdubey0017-coder <dishantdubey0017@gmail.com>
2026-08-24 11:21:35 +0530

    offer ride

--- JAVASCRIPT/dashbooard.js
+++ JAVASCRIPT/dashbooard.js
@@ -1,13 +1,14 @@
 let homePage = document.querySelector(".homepage");
 let findRidePage = document.querySelector(".findRide");
 let sidebar = document.querySelector(".sidebar")
-
+let offerRide = document.querySelector(".offerRide")
 
 sidebar.addEventListener("click",(e)=>{
     e.preventDefault();
     if(e.target.classList.contains("home-btn")){
         findRidePage.style.display = "none";
         homePage.style.display = "block";
+        offerRide.style.display = "none";
         document.querySelectorAll(".menu-item").forEach((menu)=>{
             menu.classList.remove("active")
         })
@@ -16,6 +17,17 @@ sidebar.addEventListener("click",(e)=>{
     if(e.target.classList.contains("trip-btn")){
         findRidePage.style.display = "block";
         homePage.style.display = "none";
+        offerRide.style.display = "none";
+        document.querySelectorAll(".menu-item").forEach((menu)=>{
+            menu.classList.remove("active")
+        })
+        e.target.classList.add("active")
+    }
+
+    if(e.target.classList.contains("offer-btn")){
+        findRidePage.style.display = "none";
+        homePage.style.display = "none";
+        offerRide.style.display = "block";
         document.querySelectorAll(".menu-item").forEach((menu)=>{
             menu.classList.remove("active")
         })
@@ -27,13 +39,8 @@ let user = localStorage.getItem("USER");
 user = JSON.parse(user);
 let welcomeLine = document.getElementById("welcomeLine")
 welcomeLine.innerHTML = `Welcome Back, ${user.name} 👋`;
-
 let str = "Sameer";
-
 let chr = str.split("")
-
 let userIcon = chr[0];
-
 let profileIcon = document.querySelector(".userIcon")
-
 profileIcon.innerHTML = userIcon
